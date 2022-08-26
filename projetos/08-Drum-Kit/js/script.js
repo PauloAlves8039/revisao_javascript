cd5f91076f1f0cc573a5fb9f6ea89fb2cc39b805
PauloAlves8039 <pj38alves@gmail.com>
2022-08-26 19:08:23 -0300

    Complementando lógica para execução dos sons

--- projetos/08-Drum-Kit/js/script.js
+++ projetos/08-Drum-Kit/js/script.js
@@ -23,4 +23,29 @@ const exibir = (sons) => Object.keys(sons).forEach(criarDiv);
 const tocarSom = (letra) => {
     const audio = new Audio(`../../../assets/sons/${sons[letra]}`);
     audio.play();
-}
+};
+
+const adicionarEfeito = (letra) => document.getElementById(letra).classList.toggle('active');
+
+const removerEfeito = (letra) => {
+    const div = document.getElementById(letra);
+    const removeActive = () => div.classList.remove('active');
+    div.addEventListener('transitionend',removeActive);
+};
+
+const ativarDiv = (evento) => {
+    const letra =evento.type == 'click' ? evento.target.id : evento.key.toUpperCase();
+
+    const letraPermitida = sons.hasOwnProperty(letra);
+
+    if (letraPermitida) {
+        adicionarEfeito(letra);
+        tocarSom(letra);
+        removerEfeito(letra);
+    }
+};
+
+exibir(sons);
+
+document.getElementById('container').addEventListener('click', ativarDiv);
+window.addEventListener('keyup', ativarDiv);
