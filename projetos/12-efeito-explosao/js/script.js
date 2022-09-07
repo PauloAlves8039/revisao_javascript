a15911d1901569f1a95a9de80850830ed43bee86
PauloAlves8039 <pj38alves@gmail.com>
2022-09-07 12:34:10 -0300

    Aplicando lógica para os efeitos

---
+++ projetos/12-efeito-explosao/js/script.js
@@ -0,0 +1,40 @@
+const executar = function () {
+    let checar = false;
+
+    if (checar == false) {
+        let inicioDaExecucao = setInterval(function() {inicio()}, 1000);
+        let contador = 10;
+
+        function inicio(){
+            executandoSomDoBip();
+            document.getElementById("time").innerHTML = contador;
+            if (contador == 0) {
+                clearInterval(inicioDaExecucao);
+                executandoSomDoTrovao();
+                document.getElementById("fire").src = "../../assets/gifs/explosion.gif";
+                document.getElementById("time").innerHTML = "Fim de Jogo";
+            }
+            contador --;
+        }
+        checar = true;
+
+        setTimeout(function() {fim()}, 18000);
+    }
+}
+
+const executandoSomDoTrovao = function () {
+    let bipTrovao = new Audio();
+    bipTrovao.src = "../../assets/sons/Thunder_Crack.mp3";
+    bipTrovao.play();
+}
+
+const executandoSomDoBip = function () {
+    let bip = new Audio();
+    bip.src = "../../assets/sons/Beep_Short.mp3";
+    bip.play();
+}
+
+const fim = function () {
+    document.getElementById("fire").src = "../../assets/imagens/clean.png";
+    window.location.reload();
+}
