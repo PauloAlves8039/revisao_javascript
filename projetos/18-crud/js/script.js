ea3419329b445e7421480cfadf0c0ec8be40744e
PauloAlves8039 <pj38alves@gmail.com>
2022-10-02 18:02:14 -0300

    Criando funções para lógica do crud

--- projetos/18-crud/js/script.js
+++ projetos/18-crud/js/script.js
@@ -29,3 +29,53 @@ const clearFields = function () {
     fields.forEach(field => field.value = '');
     document.getElementById('nome').dataset.index = 'new';
 }
+
+const saveClient = function () {
+    debugger
+
+    if (isValidField()){
+        const client = {
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            celular: document.getElementById('celular').value,
+            cidade: document.getElementById('cidade').value,
+        }
+
+        constindex = document.getElementById('nome').dataset.index
+
+        if (index == 'new') {
+            createCliente(client);
+            updateTable();
+            closeModel();
+        }else {
+            updateTable();
+            closeModel();
+        }
+    }
+}
+
+const createRow = function (client, index) {
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+        <td>${client.nome}</td>
+        <td>${client.email}</td>
+        <td>${client.celular}</td>
+        <td>${client.cidade}</td>
+        <td>
+            <button type="button" class="button green" id="edit-${index}">Editar</button>
+            <button type="button" class="button red" id="delete-${index}" >Excluir</button>
+        </td>
+    `
+    document.querySelector('#tableClient>tbody').appendChild(newRow)
+}
+
+const clearTable = function () {
+    const rows = document.querySelectorAll('#tableClient>tbody tr')
+    rows.forEach(row => row.parentNode.removeChild(row))
+}
+
+const updateTable = function () {
+    const dbClient = readClient()
+    clearTable()
+    dbClient.forEach(createRow)
+}
